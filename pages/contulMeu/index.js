6c28f31e84986b8d85c092bb421c35360827078a
adir2652 <79450132+adir2652@users.noreply.github.com>
2021-12-18 20:47:52 +0200

    front end David A

--- pages/contulMeu/index.js
+++ pages/contulMeu/index.js
@@ -1,10 +1,51 @@
 import Layout from '../../components/Layout'
-import  style from '../../styles/ContulMeu.module.css'
+import style from '../../styles/ContulMeu.module.css'
+import picture from '/public/3bkf81.png'
 
 export default function contulMeu() {
     return (
         <Layout>
-            <h1 className={style.titleHeader}>Aici va fi pagina pt Contul Meu</h1>
+            <div style={{marginBottom:'100px'}}>
+                <h2 align={"center"}>
+                    Informaţii
+                </h2>
+            </div>
+            <div style={{marginBottom:'100px'}}>
+                <table align={"center"}>
+                    <tr>
+                        <td rowSpan={3}>
+                            <img
+                                className={style.image}
+                                src='https://i.ibb.co/b2Yw368/3bkf81.png'
+                                alt="profile picture"
+                            />
+                        </td>
+                        <td>
+                            Nume, prenume
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Manager / Firma
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            moto-ul meu
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <h3 align={"center"}>
+                    DESPRE MINE
+                </h3>
+            </div>
+            <div style={{marginBottom:'50px'}}>
+                <p align={"justify"}>
+                    Aici începe textul. Apăsați aici și începeți să scrieți. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores
+                </p>
+            </div>
         </Layout>
     )
 }
